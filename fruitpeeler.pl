e03f4d13aae088823a3e11f5d6746a8bbb9dbbf8
bitterfruit <bitterfruit@ymail.com>
2014-05-24 14:06:32 +0200

    fix for files with $

--- fruitpeeler.pl
+++ fruitpeeler.pl
@@ -20,7 +20,7 @@ use MIME::Base64;
 
 # Global Variables
 
-$version = "0.41 (20140414)";
+$version = "0.42 (20140524)";
 $VerboseLevel = 0;  # show verbose output, 0=none, 3=shitload
 foreach (@ARGV) {
   $VerboseLevel = $1 if /^(?:--verbose=|-v)(\d+)/ && $1<4;
@@ -741,8 +741,8 @@ sub extract_selected {
     if ($isVerbosa) { print "percent_done: $percent_done\n"; } 
     $top->update();
     foreach my $pass ( @passlist ) {
-      $arg = "'".$bin{'rar'}{'path'}."'\ x -x*/Thumbs.db -p\"".escape_pass($pass)."\" -o+ -ierr \"$archpath\" \"$destpath\"" if ($archpath =~ /(\.rar)$/i);
-      $arg = "'".$bin{'zip'}{'path'}."' x -aoa -y -xr!Thumbs.db -p'$pass' \"$archpath\" -o\"$destpath\"" if ($archpath =~ /\.(7z|7z\.001|zip)$/i);
+      $arg = "'".$bin{'rar'}{'path'}."'\ x -x*/Thumbs.db -p\"".escape_pass($pass)."\" -o+ -ierr '$archpath' '$destpath'" if ($archpath =~ /(\.rar)$/i);
+      $arg = "'".$bin{'zip'}{'path'}."' x -aoa -y -xr!Thumbs.db -p'$pass' '$archpath' -o'$destpath'" if ($archpath =~ /\.(7z|7z\.001|zip)$/i);
 
       if (isCygwin() && $bin{'rar'}{'path'} !~ /unrar$|unrar\.exe$/) { # overide the above $arg if cygwin is true
         $arg = sprintf("'".$bin{'rar'}{'path'}."' x -x*\\Thumbs.db -o+ -ierr -p\"%s\" -- \"%s\" \"%s\"",
